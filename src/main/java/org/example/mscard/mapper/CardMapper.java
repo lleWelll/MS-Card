76e1fbe547284a10e4c88c9e531b28f2bdc825b2
llewell <llwll@MacBoo.local>
2025-02-24 14:13:42 +0500

    Improved card mapping: cardNumber correctly mapped when cardNumber contains "-", " ", "\t", etc. cardNumber masked in mapper

--- src/main/java/org/example/mscard/mapper/CardMapper.java
+++ src/main/java/org/example/mscard/mapper/CardMapper.java
@@ -3,13 +3,27 @@ package org.example.mscard.mapper;
 import org.example.mscard.dto.CardDTO;
 import org.example.mscard.entity.CardEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface CardMapper {
 
+    @Mapping(source = "cardNumber", target = "cardNumber", qualifiedByName = "mapCardNumber")
     CardEntity toEntity(CardDTO cardDTO);
 
-
     CardDTO toDTO(CardEntity cardEntity);
+
+    @Named("mapCardNumber")
+    static String mapCardNumber(String cardNumber) {
+        cardNumber = cardNumber.replaceAll("\\s", "");
+        cardNumber = cardNumber.replaceAll("-", "");
+        cardNumber = maskCardNumber(cardNumber);
+        return cardNumber;
+    }
+
+    static String maskCardNumber(String cardNumber) {
+        return "************" + cardNumber.substring(12);
+    }
 
 }
